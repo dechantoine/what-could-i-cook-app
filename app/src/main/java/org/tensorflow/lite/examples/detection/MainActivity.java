c9bbd5c656ff1d734a1d2a910d4d06f8e29025ec
antoine <dechantoine@gmail.com>
2019-11-15 11:42:34 +0000

    Add list View to home : still need to implement trash button

--- app/src/main/java/org/tensorflow/lite/examples/detection/MainActivity.java
+++ app/src/main/java/org/tensorflow/lite/examples/detection/MainActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ListView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -11,11 +12,18 @@ public class MainActivity extends AppCompatActivity {
 
     private Button btnRecipe;
     private Button btnCamera;
+    ListView simpleList;
+    String itemsList[] = {"Banana", "Apple"};
+    int pictures[] = {R.drawable.banana, R.drawable.apple};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        simpleList = (ListView) findViewById(R.id.listViewItems);
+        CustomAdapter customAdapter = new CustomAdapter(getApplicationContext(), itemsList, pictures);
+        simpleList.setAdapter(customAdapter);
 
         btnRecipe = (Button) findViewById(R.id.buttonRecipe);
         btnRecipe.setOnClickListener(new View.OnClickListener() {
