f282128dbad1c8098c3688b8d67381bebce5444f
Žanis <zanis.timsans@gmail.com>
2019-11-23 23:51:45 +0000

    Added image slider to Tutorial activity

--- app/src/main/java/org/tensorflow/lite/examples/detection/TutorialActivity.java
+++ app/src/main/java/org/tensorflow/lite/examples/detection/TutorialActivity.java
@@ -7,14 +7,38 @@ import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import autoimageslider.IndicatorAnimations;
+import autoimageslider.IndicatorView.draw.controller.DrawController;
+import autoimageslider.SliderAnimations;
+import autoimageslider.SliderView;
+
 public class TutorialActivity extends AppCompatActivity {
 
     private Button btnNext;
+    private SliderView sliderView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tutorial);
+
+        sliderView = findViewById(R.id.imageSlider);
+
+        final SliderAdapterExample adapter = new SliderAdapterExample(this);
+        adapter.setCount(4);
+
+        sliderView.setSliderAdapter(adapter);
+
+        sliderView.setIndicatorAnimation(IndicatorAnimations.WORM); //set indicator animation by using SliderLayout.IndicatorAnimations. :WORM or THIN_WORM or COLOR or DROP or FILL or NONE or SCALE or SCALE_DOWN or SLIDE and SWAP!!
+        sliderView.setSliderTransformAnimation(SliderAnimations.CUBEINROTATIONTRANSFORMATION);
+        sliderView.setAutoCycleDirection(SliderView.AUTO_CYCLE_DIRECTION_RIGHT);
+
+        sliderView.setOnIndicatorClickListener(new DrawController.ClickListener() {
+            @Override
+            public void onIndicatorClicked(int position) {
+                sliderView.setCurrentPagePosition(position);
+            }
+        });
 
         btnNext = (Button) findViewById(R.id.buttonNext);
         btnNext.setOnClickListener(new View.OnClickListener(){
